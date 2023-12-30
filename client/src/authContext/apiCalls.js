90e006ecb8d10ff00de258c7365ecf1f5b2753c7
shorya1104 <pratapshorya123@gmail.com>
2023-12-30 14:58:31 +0530

    first commit

---
+++ client/src/authContext/apiCalls.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
+axios = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+
+export const login = async (user, dispatch) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post("auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
